459f2ad40264cd90e0febaaf0182de6647f96271
brisa <you@example.com>
2026-05-26 05:45:59 -0600

    Guardarimagen

--- AFinalPOO/src/Escanear.java
+++ AFinalPOO/src/Escanear.java
@@ -23,160 +23,167 @@ import java.nio.file.StandardCopyOption;
 import java.awt.event.ActionEvent;
 import javax.swing.JInternalFrame;
 
-public class Escanear extends JInternalFrame {
+private static final long serialVersionUID = 1L;
+private JPanel contentPane;
+private JLabel guarda;
+private CONECTA conexionBD = new CONECTA();
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JLabel guarda;
+/**
+ * Launch the application.
+ */
+public static void main(String[] args) {
+	EventQueue.invokeLater(new Runnable() {
+		public void run() {
+			try {
+				Escanear frame = new Escanear();
+				frame.setVisible(true);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	});
+}
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Escanear frame = new Escanear();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
+/**
+ * Create the frame.
+ */
+
+
+
+public Escanear() {
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setBounds(100, 100, 283, 353);
+	contentPane = new JPanel();
+	contentPane.setBackground(new Color(255, 221, 238));
+	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	setContentPane(contentPane);
+	contentPane.setLayout(null);
+	
+	
+	
+	
+	JInternalFrame internalFrame = new JInternalFrame("New JInternalFrame");
+	internalFrame.getContentPane().setBackground(new Color(255, 225, 240));
+	internalFrame.setBounds(-13, -28, 291, 351);
+	contentPane.add(internalFrame);
+	internalFrame.getContentPane().setLayout(null);
+	JButton btnSeleccionar = new JButton("SELECCIONAR IMAGEN");
+	btnSeleccionar.setBounds(52, 219, 173, 22);
+	internalFrame.getContentPane().add(btnSeleccionar);
+	btnSeleccionar.setBackground(new Color(238, 221, 255));
+	btnSeleccionar.setFont(new Font("Yu Gothic", Font.BOLD, 12));
+	
+	 
+	guarda = new JLabel(".");
+	guarda.setBounds(41, 42, 184, 167);
+	internalFrame.getContentPane().add(guarda);
+	
+	JButton btnVOLVER = new JButton("CERRAR");
+	btnVOLVER.setBounds(181, 291, 84, 20);
+	internalFrame.getContentPane().add(btnVOLVER);
+	
+	btnVOLVER.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			
+			Escanear.this.setVisible(false);
+		}
+
+		
+			
+			
+		
+	});
+	btnVOLVER.setFont(new Font("Tahoma", Font.ITALIC, 10));
+	btnVOLVER.setBackground(new Color(236, 217, 255));
+	
+	JLabel lblEscanear = new JLabel("CARGAR IMAGEN");
+	lblEscanear.setBounds(77, 10, 125, 22);
+	internalFrame.getContentPane().add(lblEscanear);
+	lblEscanear.setForeground(new Color(128, 0, 128));
+	lblEscanear.setFont(new Font("Yu Gothic", Font.BOLD, 13));
+	
+	JButton btnGuardarImg = new JButton("GUARDAR");
+	btnGuardarImg.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+
+	        ImageIcon icon = (ImageIcon) guarda.getIcon();
+
+	        String link = icon.getDescription();
+
+	        boolean respuesta = conexionBD.guardarImagen(link);
+
+	        if(respuesta) {
+
+	            JOptionPane.showMessageDialog(null, "Imagen guardada");
+
+	        } else {
+
+	            JOptionPane.showMessageDialog(null, "Error al guardar");
+	        }
+	    
+			
+		}
+	});
+	btnGuardarImg.setFont(new Font("Yu Gothic", Font.BOLD, 12));
+	btnGuardarImg.setBackground(new Color(236, 217, 255));
+	btnGuardarImg.setBounds(52, 251, 173, 22);
+	internalFrame.getContentPane().add(btnGuardarImg);
+	internalFrame.setVisible(true);
+	
+	btnSeleccionar.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent e) {
+	        btnSeleccionarArchivoActionPerformed(e);
+	    }
+	});
+	
+	
+		  
+	
+}
+	//Aquí se carga la imágen desde el botón
+	private void btnSeleccionarArchivoActionPerformed(java.awt.event.ActionEvent evt) {
+	    JFileChooser fileChooser = new JFileChooser();
+	    // Filtro para solo mostrar imágenes
+	    fileChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("Imágenes", "jpg", "png", "gif"));
+	    
+	    int resultado = fileChooser.showOpenDialog(this);
+	    
+	    if (resultado == JFileChooser.APPROVE_OPTION) {
+	        File archivoSeleccionado = fileChooser.getSelectedFile();
+	        try {
+	            ImageIcon icon = new ImageIcon(archivoSeleccionado.getAbsolutePath());
+	          
+				// Mostrar en etiqueta o botón
+	            guarda.setIcon(icon); 
+	        } catch (Exception ex) {
+	        	 Path origen = archivoSeleccionado.toPath();
+
+	             // Nombre del archivo
+	             String nombreImagen = archivoSeleccionado.getName();
+
+	             // Carpeta destino
+	             Path destino = Paths.get("imagenes/" + nombreImagen);
+
+	             // Copiar imagen a la carpeta imagenes
+	             try {
+					Files.copy(
+					     origen,
+					     destino,
+					     StandardCopyOption.REPLACE_EXISTING 
+					     );
+				       // Mostrar imagen guardada
+		            ImageIcon icon = new ImageIcon(destino.toString());
+
+		            guarda.setIcon(icon);
+
+		            JOptionPane.showMessageDialog(this, "Imagen guardada en la carpeta imagenes");
+
+		        } catch (Exception em) {
+
+		            JOptionPane.showMessageDialog(this,"Error: " + em.getMessage());
 				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
+	        }
+	    }
 	
-	
-	
-	public Escanear() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 283, 353);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(255, 221, 238));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		
-		
-		
-		JInternalFrame internalFrame = new JInternalFrame("New JInternalFrame");
-		internalFrame.getContentPane().setBackground(new Color(255, 225, 240));
-		internalFrame.setBounds(-13, -28, 291, 351);
-		contentPane.add(internalFrame);
-		internalFrame.getContentPane().setLayout(null);
-		JButton btnSeleccionar = new JButton("SELECCIONAR IMAGEN");
-		btnSeleccionar.setBounds(52, 219, 173, 22);
-		internalFrame.getContentPane().add(btnSeleccionar);
-		btnSeleccionar.setBackground(new Color(238, 221, 255));
-		btnSeleccionar.setFont(new Font("Yu Gothic", Font.BOLD, 12));
-		
-		 
-		guarda = new JLabel(".");
-		guarda.setBounds(41, 42, 184, 167);
-		internalFrame.getContentPane().add(guarda);
-		
-		JButton btnVOLVER = new JButton("CERRAR");
-		btnVOLVER.setBounds(181, 291, 84, 20);
-		internalFrame.getContentPane().add(btnVOLVER);
-		btnVOLVER.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				Escanear.this.setVisible(false);
-			}
-
-			
-				
-				
-			
-		});
-		btnVOLVER.setFont(new Font("Tahoma", Font.ITALIC, 10));
-		btnVOLVER.setBackground(new Color(236, 217, 255));
-		
-		JLabel lblEscanear = new JLabel("CARGAR IMAGEN");
-		lblEscanear.setBounds(77, 10, 125, 22);
-		internalFrame.getContentPane().add(lblEscanear);
-		lblEscanear.setForeground(new Color(128, 0, 128));
-		lblEscanear.setFont(new Font("Yu Gothic", Font.BOLD, 13));
-		
-		JButton btnGuardarImg = new JButton("GUARDAR");
-		btnGuardarImg.setFont(new Font("Yu Gothic", Font.BOLD, 12));
-		btnGuardarImg.setBackground(new Color(236, 217, 255));
-		btnGuardarImg.setBounds(52, 251, 173, 22);
-		internalFrame.getContentPane().add(btnGuardarImg);
-		internalFrame.setVisible(true);
-		
-		btnSeleccionar.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {
-		    	
-
-		    			    JFileChooser fileChooser =
-		    			    new JFileChooser();
-
-		    			    int resultado =fileChooser.showOpenDialog(Escanear.this);
-
-		    			    if(resultado ==
-		    			       JFileChooser.APPROVE_OPTION) {
-
-		    			        File archivoSeleccionado =
-		    			        fileChooser.getSelectedFile();
-
-		    			        try {
-
-		    			            File carpeta =
-		    			            new File("imagenes");
-
-		    			            if(!carpeta.exists()) {
-
-		    			                carpeta.mkdir();
-		    			            }
-
-		    			            Path origen =
-		    			            archivoSeleccionado.toPath();
-
-		    			            String nombre =
-		    			            archivoSeleccionado.getName();
-
-		    			            Path destino =
-		    			            Paths.get("imagenes/" + nombre);
-
-		    			            Files.copy(
-		    			                origen,
-		    			                destino,
-		    			                StandardCopyOption.REPLACE_EXISTING
-		    			            );
-
-		    			            ImageIcon icon =
-		    			            new ImageIcon(destino.toString());
-
-		    			            guarda.setIcon(icon);
-
-		    			            JOptionPane.showMessageDialog(
-		    			            	    Escanear.this,
-		    			            	    "Imagen guardada"
-		    			            	
-		    			            );
-
-		    			        } catch(Exception e1) {
-
-		    			        	JOptionPane.showMessageDialog(
-		    			        		    null,
-		    			        		    e1.getMessage()
-		    			            );
-		    			        }
-		    			    }
-		    			}
-		    
-		});
-		
-		
-			  
-		
-	}
-
-		
-	}
+}
 
--- AFinalPOO/src/src/CONECTA.java
+++ AFinalPOO/src/src/CONECTA.java
@@ -9,160 +9,182 @@ public class CONECTA {
 	
 
 	
-			static String url1 = "jdbc:postgresql://aws-1-us-east-1.pooler.supabase.com:6543/postgres";
-            static String user = "postgres.phxwmwznpuxrjhnftbid";
-            static String password = "o7Sp7ifonr29L5D6";
-            public static Connection getConnection() throws Exception {
-        		Class.forName("org.postgresql.Driver");
-        		return DriverManager.getConnection(url1, user, password);
-        	}
-
-        	public static void main(String[] args) {
-        		try {
-
-        			Connection MyConn = getConnection();
-        			Statement myStmt = MyConn.createStatement();
-        			ResultSet myRs = myStmt.executeQuery("select * from fecha");
-        			while(myRs.next()) {
-        				System.out.println(myRs.getString("evento"));
-        			}
-
-        		} catch(Exception e) {
-        			e.printStackTrace();
-        		}
-        	}
-		
-
-
-        	public ResultSet getFechas() {
-        	    try {
-        	        // Usamos las variables url1, user y password que ya definiste en la clase
-        	        Connection MyConn = DriverManager.getConnection(url1, user, password);
-        	        // IMPORTANTE: TYPE_SCROLL_INSENSITIVE para que funcione .previous()
-        	        Statement myStmt = MyConn.createStatement(
-        	            ResultSet.TYPE_SCROLL_INSENSITIVE, 
-        	            ResultSet.CONCUR_READ_ONLY
-        	        );
-        	        return myStmt.executeQuery("select * from fecha");
-        	    } catch (SQLException e) {
-        	        e.printStackTrace();
-        	        return null; // Si falla, devuelve null
-        	    }
-        	}
-
-	public boolean guardarFecha(String evento, int dia, String mes,
-			String anio, String hora, String minutos) {
-
-				try {
-					 String url1 = "jdbc:postgresql://aws-1-us-east-1.pooler.supabase.com:6543/postgres";
-			            String user = "postgres.phxwmwznpuxrjhnftbid";
-			            String password = "o7Sp7ifonr29L5D6";
-					Connection MyConn =DriverManager.getConnection(url1, user, password);
-
-					Statement myStmt = MyConn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
-
-					String sql = "Insert into fecha "
-							+ "(evento,dia,mes,anio,hora,minutos) "
-							+ "values('"+evento+"',"+dia+",'"
-							+ mes+"','"+anio+"','"
-							+ hora+"','"+minutos+"')";
-
-					myStmt.executeUpdate(sql);
-
-					return true;
-
-				} catch(SQLException e) {
-
-					e.printStackTrace();
-
-					return false;
-				}
-			}
-	
-	public boolean borrarFecha(String evento,int dia, String mes,
-			String anio, String hora, String minutos) {
-	    try {
-	        String url1 = "jdbc:postgresql://aws-1-us-east-1.pooler.supabase.com:6543/postgres";
-	        String user = "postgres.phxwmwznpuxrjhnftbid";
-	        String password = "o7Sp7ifonr29L5D6";
-	        Connection MyConn = DriverManager.getConnection(url1, user, password);
-	        
-	        Statement myStmt = MyConn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-	        
-	        String sql = "DELETE FROM fecha "+ "WHERE evento='" + evento + "'"+ " AND dia=" + dia+ " AND mes='" + mes + "'" + " AND anio='" + anio + "'"
-	                + " AND hora='" + hora + "'"
-	                + " AND minutos='" + minutos + "'";
-	        
-	        
-	        myStmt.executeUpdate(sql);
-	        
-	        
-	        return true;
-	        
-	        
-	        
-	    } catch(SQLException e) {
-	        e.printStackTrace();
-	        return false;
-	    }
-	}
-	public boolean borrarNota(String nota) {
-	    try {
-	        Connection MyConn = DriverManager.getConnection(url1, user, password);
-	        Statement myStmt = MyConn.createStatement();
-	        
-	        String sql = "DELETE FROM notas "
-	                + "WHERE texto='" + nota + "'";
-	        
-	        myStmt.executeUpdate(sql);
-	        return true;
-	        
-	        
-	    } catch(SQLException e) {
-	        e.printStackTrace();
-	        return false;
-	    }
+	static String url1 = "jdbc:postgresql://aws-1-us-east-1.pooler.supabase.com:6543/postgres";
+    static String user = "postgres.phxwmwznpuxrjhnftbid";
+    static String password = "o7Sp7ifonr29L5D6";
+    public static Connection getConnection() throws Exception {
+		Class.forName("org.postgresql.Driver");
+		return DriverManager.getConnection(url1, user, password);
 	}
 
-	public boolean guardarNota(String nota) {
-
+	public static void main(String[] args) {
 		try {
 
-			Connection MyConn = DriverManager.getConnection(url1,user,password);
+			Connection MyConn = getConnection();
 			Statement myStmt = MyConn.createStatement();
-			String sql = "insert into notas (texto) "
-			+ "values('"+nota+"')";
-			System.out.println(sql);
-			
+			ResultSet myRs = myStmt.executeQuery("select * from fecha");
+			while(myRs.next()) {
+				System.out.println(myRs.getString("evento"));
+			}
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+	public ResultSet getFechas() {
+	    try {
+	        // Usamos las variables url1, user y password que ya definiste en la clase
+	        Connection MyConn = DriverManager.getConnection(url1, user, password);
+	        // IMPORTANTE: TYPE_SCROLL_INSENSITIVE para que funcione .previous()
+	        Statement myStmt = MyConn.createStatement(
+	            ResultSet.TYPE_SCROLL_INSENSITIVE, 
+	            ResultSet.CONCUR_READ_ONLY
+	        );
+	        return myStmt.executeQuery("select * from fecha");
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        return null; // Si falla, devuelve null
+	    }
+	}
+	
+	
+
+public boolean guardarFecha(String evento, int dia, String mes,
+	String anio, String hora, String minutos) {
+
+		try {
+			 String url1 = "jdbc:postgresql://aws-1-us-east-1.pooler.supabase.com:6543/postgres";
+	            String user = "postgres.phxwmwznpuxrjhnftbid";
+	            String password = "o7Sp7ifonr29L5D6";
+			Connection MyConn =DriverManager.getConnection(url1, user, password);
+
+			Statement myStmt = MyConn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+
+			String sql = "Insert into fecha "
+					+ "(evento,dia,mes,anio,hora,minutos) "
+					+ "values('"+evento+"',"+dia+",'"
+					+ mes+"','"+anio+"','"
+					+ hora+"','"+minutos+"')";
+
 			myStmt.executeUpdate(sql);
+
 			return true;
 
 		} catch(SQLException e) {
+
 			e.printStackTrace();
+
 			return false;
-		}}
-	
-	
-	
-		public ResultSet getNotas() {
-		    try {
-		        Connection MyConn = DriverManager.getConnection(url1, user, password);
-		        Statement myStmt = MyConn.createStatement(
-		            ResultSet.TYPE_SCROLL_INSENSITIVE, 
-		            ResultSet.CONCUR_READ_ONLY
-		        );
-		        
-		        // AL PEDIR "texto" PRIMERO, EL ÍNDICE 1 SIEMPRE SERÁ TU NOTA
-		        return myStmt.executeQuery("SELECT texto FROM notas"); 
-		        
-		    } catch (SQLException e) {
-		        e.printStackTrace();
-		        return null;
-		    }
+		}
 	}
+public boolean borrarFecha(String evento,int dia, String mes,
+	String anio, String hora, String minutos) {
+try {
+    String url1 = "jdbc:postgresql://aws-1-us-east-1.pooler.supabase.com:6543/postgres";
+    String user = "postgres.phxwmwznpuxrjhnftbid";
+    String password = "o7Sp7ifonr29L5D6";
+    Connection MyConn = DriverManager.getConnection(url1, user, password);
+    
+    Statement myStmt = MyConn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+    
+    String sql = "DELETE FROM fecha "+ "WHERE evento='" + evento + "'"+ " AND dia=" + dia+ " AND mes='" + mes + "'" + " AND anio='" + anio + "'"
+            + " AND hora='" + hora + "'"
+            + " AND minutos='" + minutos + "'";
+    
+    
+    myStmt.executeUpdate(sql);
+    
+    
+    return true;
+    
+    
+    
+} catch(SQLException e) {
+    e.printStackTrace();
+    return false;
+}
+}
+public boolean borrarNota(String nota) {
+try {
+    Connection MyConn = DriverManager.getConnection(url1, user, password);
+    Statement myStmt = MyConn.createStatement();
+    
+    String sql = "DELETE FROM notas "
+            + "WHERE texto='" + nota + "'";
+    
+    myStmt.executeUpdate(sql);
+    return true;
+    
+    
+} catch(SQLException e) {
+    e.printStackTrace();
+    return false;
+}
+}
+
+public boolean guardarImagen(String ruta) {
+try {
+    String url1 = "jdbc:postgresql://aws-1-us-east-1.pooler.supabase.com:6543/postgres";
+    String user = "postgres.phxwmwznpuxrjhnftbid";
+    String password = "o7Sp7ifonr29L5D6";
+    Connection MyConn = DriverManager.getConnection(url1, user, password);
+    
+    Statement myStmt = MyConn.createStatement();
+    String sql = "INSERT INTO imagenes (link) "
+            + "values('" + ruta + "')";
+    
+    myStmt.executeUpdate(sql);
+    
+    return true;
+    
+} catch(SQLException e) {
+    e.printStackTrace();
+    return false;
+}
+}
+
+public boolean guardarNota(String nota) {
+
+try {
+
+	Connection MyConn = DriverManager.getConnection(url1,user,password);
+	Statement myStmt = MyConn.createStatement();
+	String sql = "insert into notas (texto) "
+	+ "values('"+nota+"')";
+	System.out.println(sql);
 	
-	
-	
-	}
+	myStmt.executeUpdate(sql);
+	return true;
+
+} catch(SQLException e) {
+	e.printStackTrace();
+	return false;
+}}
+
+
+
+public ResultSet getNotas() {
+    try {
+        Connection MyConn = DriverManager.getConnection(url1, user, password);
+        Statement myStmt = MyConn.createStatement(
+            ResultSet.TYPE_SCROLL_INSENSITIVE, 
+            ResultSet.CONCUR_READ_ONLY
+        );
+        
+        // AL PEDIR "texto" PRIMERO, EL ÍNDICE 1 SIEMPRE SERÁ TU NOTA
+        return myStmt.executeQuery("SELECT texto FROM notas"); 
+        
+    } catch (SQLException e) {
+        e.printStackTrace();
+        return null;
+    }
+}
+
+
+
+}
 
 	
